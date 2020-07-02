47d098913fd091f41fcdefb7f0f60c1080c233bf
Matheus R <01mrsantos@gmail.com>
2020-07-02 05:34:24 -0300

    changes in install.sh

--- myscripts/install.sh
+++ myscripts/install.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 mkdir -p ~/.myscripts
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/polybar
+mkdir -p ~/.config/rofi
+mkdir -p ~/.config/wal/templates
+mkdir -p ~/.config/zathura
+mkdir -p ~/.cache/wal
 
 ln -s ~/.dotfiles/Wallpapers ~/Pictures/Wallpapers
 ln -s ~/.dotfiles/i3config ~/.config/i3/config
 ln -s ~/.dotfiles/lock.png ~/.config/lock.png
-ln -s ~/.dotfiles/myscripts ~/.myscripts
+ln -s ~/.dotfiles/myscripts/* ~/.myscripts/
 ln -s ~/.dotfiles/picom.conf ~/.config/picom.conf
 ln -s ~/.dotfiles/polybar ~/.config/polybar/config
 ln -s ~/.dotfiles/rofi/config.rasi ~/.config/rofi/config.rasi
@@ -16,4 +22,4 @@ ln -s ~/.dotfiles/Xresources ~/.Xresources
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 
 ln ~/.cache/wal/colors-konsole.colorscheme ~/.local/share/konsole
-ln .cache/wal/colors-zathura .config/zathura/zathurarc
+ln ~/.cache/wal/colors-zathura .config/zathura/zathurarc
