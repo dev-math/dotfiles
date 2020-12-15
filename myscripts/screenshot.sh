017250896109b9b0405f7989720038f5b147fa2d
Matheus Rocha <01mrsantos@gmail.com>
2020-12-15 03:58:06 -0300

    update for thinkpad t470

--- myscripts/screenshot.sh
+++
@@ -1,5 +0,0 @@
-NAME=~/Pictures/screenshots/$(date '+%d-%m-%Y-%T').png
-if maim -s $NAME; then
-    xclip -selection clipboard -t image/png -filter $NAME
-    notify-send "Screenshot saved at $NAME" --icon=camera-photo
-fi
