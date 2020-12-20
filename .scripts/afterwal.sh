b4ff27e93627d1ecba005c5dd181779ebbd89d3c
Matheus Rocha <01mrsantos@gmail.com>
2020-12-20 19:33:40 -0300

    double titlebar removed

---
+++ .scripts/afterwal.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rm $HOME/.Xresources
+cp $HOME/.cache/wal/.Xresources $HOME
+xrdb -merge ~/.Xresources
+
+rm $HOME/.dotfiles/.scripts/lock
+cp $HOME/.cache/wal/lock-alpha.sh $HOME/.dotfiles/.scripts/lock
+chmod +x $HOME/.dotfiles/.scripts/lock 
+
+spicetify update
+
+pywal-discord
+rm $HOME/.config/zathura/zathurarc
+cp $HOME/.cache/wal/zathurarc $HOME/.config/zathura/zathurarc
+
+/opt/oomox/plugins/icons_papirus/change_color.sh /opt/oomox/scripted_colors/xresources/xresources-reverse
+
+oomox-cli /home/math/.config/oomox/colors/xresources-with-papirus
+
+# echo 'awesome.restart()' | awesome-client &> /dev/null
