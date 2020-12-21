4723595914ec5fb33d669c99c2f14036e36426f2
Matheus Rocha <01mrsantos@gmail.com>
2020-12-20 22:42:51 -0300

    removed useless lines

--- .scripts/afterwal.sh
+++ .scripts/afterwal.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+. $HOME/.cache/wal/colors.sh
 
 rm $HOME/.Xresources
 cp $HOME/.cache/wal/.Xresources $HOME
 xrdb -merge ~/.Xresources
+feh --bg-fill "$wallpaper"
+
+echo 'awesome.restart()' | awesome-client &> /dev/null
 
 rm $HOME/.dotfiles/.scripts/lock
 cp $HOME/.cache/wal/lock-alpha.sh $HOME/.dotfiles/.scripts/lock
@@ -11,10 +15,9 @@ chmod +x $HOME/.dotfiles/.scripts/lock
 spicetify update
 
 pywal-discord
+
 rm $HOME/.config/zathura/zathurarc
 cp $HOME/.cache/wal/zathurarc $HOME/.config/zathura/zathurarc
 
-/opt/oomox/plugins/icons_papirus/change_color.sh /opt/oomox/scripted_colors/xresources/xresources-reverse
+# /opt/oomox/plugins/icons_papirus/change_color.sh /opt/oomox/scripted_colors/xresources/xresources-reverse
 oomox-cli /opt/oomox/scripted_colors/xresources/xresources-reverse
-
-echo 'awesome.restart()' | awesome-client &> /dev/null
