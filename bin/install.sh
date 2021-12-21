80bf77341ad338db660d8274231d4ed39fb2662d
Matheus Rocha <01mrsantos@gmail.com>
2021-12-21 19:22:08 -0300

    Fixed: spaceship theme install

--- bin/install.sh
+++ bin/install.sh
@@ -244,9 +244,9 @@ esac
 # install oh-my-zsh
 ! [ -e ~/.oh-my-zsh ] && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 # install spaceship theme
-! [ -e $ZSH_CUSTOM/themes/spaceship-prompt ] && sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-sudo rm -f "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-sudo ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+! [ -e $ZSH/custom/themes/spaceship-prompt ] && sudo git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt" --depth=1
+sudo rm -f "$ZSH/custom/themes/spaceship.zsh-theme"
+sudo ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
 # install zinit
 sh -c "$(curl -fsSL https://git.io/zinit-install)"
 # zsh cfg file
