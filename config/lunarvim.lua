a6ecffdb6b42ff20364ea18f527306558cdae705
Matheus Rocha <01devmath@gmail.com>
2022-06-27 07:24:37 -0300

    cmp: only confirm explicitly selected items

--- config/lunarvim.lua
+++ config/lunarvim.lua
@@ -196,7 +196,7 @@ lvim.builtin.cmp = {
     ["<C-e>"] = cmp.mapping.close(),
     ["<CR>"] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+      select = false,
     },
     ["<Tab>"] = function(fallback)
       if cmp.visible() then
