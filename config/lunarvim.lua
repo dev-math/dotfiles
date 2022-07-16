3fb41bca506a01a0ef22a344e2e73dda37bbd19b
Matheus Rocha <01devmath@gmail.com>
2022-07-16 18:33:20 -0300

    cmp config

--- config/lunarvim.lua
+++ config/lunarvim.lua
@@ -171,7 +171,10 @@ else
   require("luasnip/loaders/from_vscode").lazy_load()
 end
 
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require "cmp"
+cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
 lvim.builtin.cmp = {
   snippet = {
     expand = function(args)
