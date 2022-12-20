b5e3c406c495eb0f2af77aafa08a52f6adfba564
Matheus Rocha <01devmath@gmail.com>
2022-12-19 21:49:14 -0300

    Enable coq nvim

--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -3,21 +3,65 @@ if not ok then
   return
 end
 
-autopairs.setup({
-  check_ts = true,
-  ts_config = {
-    lua = { 'string', 'source' },
-    javascript = { 'string', 'template_string' },
-    java = false,
-  },
-  disable_filetype = { 'TelescopePrompt', 'vim' },
-  fast_wrap = {},
-})
-
 local cmp_status_ok, cmp = pcall(require, "cmp")
 if not cmp_status_ok then
-  return
-end
-local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+  local remap = vim.api.nvim_set_keymap
+  autopairs.setup({
+    check_ts = true,
+    ts_config = {
+      lua = { 'string', 'source' },
+      javascript = { 'string', 'template_string' },
+      java = false,
+    },
+    disable_filetype = { 'TelescopePrompt', 'vim' },
+    fast_wrap = {},
+    map_bs = false,
+    map_cr = false
+  })
 
-cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+  vim.g.coq_settings = { keymap = { recommended = false } }
+
+  -- these mappings are coq recommended mappings unrelated to nvim-autopairs
+  remap('i', '<esc>', [[pumvisible() ? "<c-e><esc>" : "<esc>"]], { expr = true, noremap = true })
+  remap('i', '<c-c>', [[pumvisible() ? "<c-e><c-c>" : "<c-c>"]], { expr = true, noremap = true })
+  remap('i', '<tab>', [[pumvisible() ? "<c-n>" : "<tab>"]], { expr = true, noremap = true })
+  remap('i', '<s-tab>', [[pumvisible() ? "<c-p>" : "<bs>"]], { expr = true, noremap = true })
+
+  -- skip it, if you use another global object
+  _G.MUtils = {}
+
+  MUtils.CR = function()
+    if vim.fn.pumvisible() ~= 0 then
+      if vim.fn.complete_info({ 'selected' }).selected ~= -1 then
+        return autopairs.esc('<c-y>')
+      else
+        return autopairs.esc('<c-e>') .. autopairs.autopairs_cr()
+      end
+    else
+      return autopairs.autopairs_cr()
+    end
+  end
+  remap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true })
+
+  MUtils.BS = function()
+    if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ 'mode' }).mode == 'eval' then
+      return autopairs.esc('<c-e>') .. autopairs.autopairs_bs()
+    else
+      return autopairs.autopairs_bs()
+    end
+  end
+  remap('i', '<bs>', 'v:lua.MUtils.BS()', { expr = true, noremap = true })
+else
+  autopairs.setup({
+    check_ts = true,
+    ts_config = {
+      lua = { 'string', 'source' },
+      javascript = { 'string', 'template_string' },
+      java = false,
+    },
+    disable_filetype = { 'TelescopePrompt', 'vim' },
+    fast_wrap = {},
+  })
+  local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+end
