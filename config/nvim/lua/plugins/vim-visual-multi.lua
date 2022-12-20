d44479d0e9464e26b54fa2037fd68e934211862e
Matheus Rocha <01devmath@gmail.com>
2022-12-20 01:59:46 -0300

    Remove useless multicursors binds

--- config/nvim/lua/plugins/vim-visual-multi.lua
+++ config/nvim/lua/plugins/vim-visual-multi.lua
@@ -1,6 +1,13 @@
--- Add selection to next find match (leaderkey + s)
 vim.cmd [[
-  let g:VM_maps = {}
-  let g:VM_maps['Find Under']         = '<leader>s'
-  let g:VM_maps['Find Subword Under'] = '<leader>s'
+let g:VM_maps = {}
+let g:VM_default_mappings = 0
+let g:VM_mouse_mappings = 1
+let g:VM_maps['Find Under'] = '<leader>s'
+let g:VM_maps['Find Subword Under'] = '<leader>s'
+let g:VM_maps["Select All"] = '<A-n>'  
+let g:VM_maps["Skip Region"] = '<C-n>' 
+let g:VM_maps["Select Cursor Down"] = '<M-C-Down>'
+let g:VM_maps["Select Cursor Up"] = '<M-C-Up>'
+let g:VM_maps["Find Next"] = 'n'
+let g:VM_maps["Find Prev"] = 'N'
 ]]
