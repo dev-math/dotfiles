f20cc0446c6b7b9b9c7dcecc1588828dba15cda4
Matheus Rocha <01devmath@gmail.com>
2022-12-25 23:02:35 -0300

    fix: (mason) lsp doesn't start

--- config/nvim/lua/lsp/lspconfig.lua
+++ config/nvim/lua/lsp/lspconfig.lua
@@ -10,22 +10,23 @@ end
 
 local u = require('core.utils')
 local default_config = require('lsp.servers.default')
+local config = default_config
 
-for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
-  local config = default_config
+mason_lspconfig.setup_handlers({
+  function (server_name) -- default handler (optional)
+    if server_name == 'jsonls' then
+      config = u.merge(config, require('lsp.servers.jsonls'))
+    elseif server_name == 'sumneko_lua' then
+      config = u.merge(config, require('lsp.servers.sumneko_lua'))
+    elseif server_name == 'tsserver' then
+      config = u.merge(config, require('lsp.servers.tsserver'))
+    end
 
-  if server.name == 'jsonls' then
-    config = u.merge(config, require('lsp.servers.jsonls'))
-  elseif server.name == 'sumneko_lua' then
-    config = u.merge(config, require('lsp.servers.sumneko_lua'))
-  elseif server.name == 'tsserver' then
-    config = u.merge(config, require('lsp.servers.tsserver'))
-  end
-
-  local status_ok, coq = pcall(require, "coq")
-  if not status_ok then
-    lspconfig[server.name].setup(config)
-  else
-    lspconfig[server.name].setup(coq.lsp_ensure_capabilities(config))
-  end
-end
+    local status_ok, coq = pcall(require, "coq")
+    if not status_ok then
+      lspconfig[server_name].setup(config)
+    else
+      lspconfig[server_name].setup(coq.lsp_ensure_capabilities(config))
+    end
+  end,
+})
