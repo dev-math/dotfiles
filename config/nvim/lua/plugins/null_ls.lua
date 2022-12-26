f78426d281dadd8e315f2431cd45066da173b582
Matheus Rocha <01devmath@gmail.com>
2022-12-26 00:24:44 -0300

    feat: use prettier and eslint for linting and formatting

--- config/nvim/lua/plugins/null_ls.lua
+++ config/nvim/lua/plugins/null_ls.lua
@@ -8,17 +8,26 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
--- https://github.com/prettier-solidity/prettier-plugin-solidity
+local code_actions = null_ls.builtins.code_actions
+
 null_ls.setup {
   debug = false,
   sources = {
-    formatting.prettier.with {
-      extra_filetypes = { "toml" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-    },
-    formatting.black.with { extra_args = { "--fast" } },
+    formatting.prettierd,
     formatting.stylua,
-    -- formatting.google_java_format,
-    diagnostics.flake8,
+    formatting.eslint_d,
+    code_actions.eslint_d,
+    diagnostics.eslint_d,
   },
+}
+
+local ok, mason_null_ls = pcall(require, "mason-null-ls")
+if not ok then
+  return
+end
+
+mason_null_ls.setup {
+  ensure_installed = nil,
+  automatic_installation = true,
+  automatic_setup = false,
 }
