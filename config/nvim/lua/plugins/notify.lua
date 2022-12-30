c306112c72531011de1de8adfae9000efb524fbc
Matheus Rocha <01devmath@gmail.com>
2022-12-30 11:17:40 -0300

    feat: <ESC> clear out notification

--- config/nvim/lua/plugins/notify.lua
+++ config/nvim/lua/plugins/notify.lua
@@ -1,13 +1,14 @@
 local ok, notify = pcall(require, "notify")
 if not ok then
-  return
+	return
 end
 
 local config = {
-  background_colour = '#000000',
-  stages = "fade",
-  timeout = 5000,
+	background_colour = "#000000",
+	stages = "fade",
+	timeout = 5000,
 }
 
 notify.setup(config)
 vim.notify = notify
+vim.keymap.set("", "<Esc>", "<ESC>:noh<CR>:lua require('notify').dismiss()<CR>", { silent = true })
