ce931dbdd61d5055b43ada3e0082b76807d9d640
Matheus Rocha <01devmath@gmail.com>
2023-01-01 14:50:16 -0300

    feat: enable prettier_eslint source on null_ls

--- config/nvim/lua/plugins/null_ls.lua
+++ config/nvim/lua/plugins/null_ls.lua
@@ -1,42 +1,47 @@
 local ok, null_ls = pcall(require, "null-ls")
 if not ok then
-  return
+	return
 end
 
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
 local formatting = null_ls.builtins.formatting
--- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
-
 local code_actions = null_ls.builtins.code_actions
 
-null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.clang_format,
-    formatting.prettierd.with({
-      prefer_local = "node_modules/.bin",
-    }),
-    formatting.stylua,
-    formatting.eslint_d.with({
-      prefer_local = "node_modules/.bin",
-    }),
-    code_actions.eslint_d({
-      prefer_local = "node_modules/.bin",
-    }),
-    diagnostics.eslint_d({
-      prefer_local = "node_modules/.bin",
-    }),
-  },
+local eslint_config = {
+	prefer_local = "node_modules/.bin",
+	condition = function(utils)
+		return utils.root_has_file({
+			".eslintrc",
+			".eslintrc.js",
+			".eslintrc.cjs",
+			".eslintrc.yaml",
+			".eslintrc.yml",
+			".eslintrc.json",
+		})
+	end,
 }
+
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.clang_format,
+		formatting.prettierd.with({
+			prefer_local = "node_modules/.bin",
+		}),
+		formatting.stylua,
+		formatting.prettier_eslint.with(eslint_config),
+		code_actions.eslint_d.with(eslint_config),
+		diagnostics.eslint_d.with(eslint_config),
+	},
+})
 
 local ok, mason_null_ls = pcall(require, "mason-null-ls")
 if not ok then
-  return
+	return
 end
 
-mason_null_ls.setup {
-  ensure_installed = nil,
-  automatic_installation = true,
-  automatic_setup = false,
-}
+mason_null_ls.setup({
+	ensure_installed = nil,
+	automatic_installation = true,
+	automatic_setup = false,
+})
