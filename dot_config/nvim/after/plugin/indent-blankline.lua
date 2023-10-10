1fb6cc645b6c3a282b390d6227f15876928b862f
Matheus Rocha <01devmath@gmail.com>
2023-10-09 23:30:01 -0300

    fix: indent blankline new syntax

--- dot_config/nvim/after/plugin/indent-blankline.lua
+++ dot_config/nvim/after/plugin/indent-blankline.lua
@@ -1,23 +1,23 @@
-local ok, indent_blankline = pcall(require, "indent_blankline")
+local ok, indent_blankline = pcall(require, "ibl")
 if not ok then
   return
 end
 
 indent_blankline.setup {
-  indentLine_enabled = 1,
-  -- char = "▏",
-  char = "┊",
-  filetype_exclude = {
-    "help",
-    "alpha",
-    "terminal",
-    "dashboard",
-    "packer",
-    "lspinfo",
-    "TelescopePrompt",
-    "TelescopeResults",
+  indent = {
+    char = "┊"
   },
-  buftype_exclude = { "terminal" },
-  show_trailing_blankline_indent = false,
-  show_first_indent_level = false,
+  exclude = {
+    filetypes = {
+      "help",
+      "alpha",
+      "terminal",
+      "dashboard",
+      "packer",
+      "lspinfo",
+      "TelescopePrompt",
+      "TelescopeResults",
+    },
+    buftypes = { "terminal" },
+  }
 }
