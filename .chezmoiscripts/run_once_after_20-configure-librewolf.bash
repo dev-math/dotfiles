9cd477d9ca4237a54f891ff6eb5e07fbbe109bba
Matheus Rocha <01devmath@gmail.com>
2023-12-20 23:38:05 -0300

    feat: add librewolf config

---
+++ .chezmoiscripts/run_once_after_20-configure-librewolf.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit -o nounset
+# Start librewolf in headless mode in order to create config directory for it.
+mkdir -p $HOME/.librewolf
+librewolf --headless >/dev/null 2>&1 &
+sleep 3
+killall librewolf
+
+# Find the profile folder.
+LIBREW_CONFIG_DIR="$HOME/.librewolf"
+LIBREW_PROF_NAME="$(sed -n "/Default=.*.default-release/ s/.*=//p" "$LIBREW_CONFIG_DIR/profiles.ini")"
+LIBREW_CHROME_DIR="$LIBREW_CONFIG_DIR/$LIBREW_PROF_NAME/chrome"
+
+mkdir -p "$LIBREW_CHROME_DIR"
+mv "$CHEZMOI_SOURCE_DIR/dot_config/userChrome.css" "$LIBREW_CHROME_DIR"
+
+# Configure mpv firefox add-on
+xdg-mime default mpv-scheme-handler.desktop x-scheme-handler/mpv
