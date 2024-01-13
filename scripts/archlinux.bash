2bfe49e87966f371fdc85a955a73a3251fa9d4bf
Matheus Rocha <01devmath@gmail.com>
2024-01-13 02:15:06 -0300

    feat: configure and set i3wm

--- scripts/archlinux.bash
+++ scripts/archlinux.bash
@@ -20,23 +20,22 @@ if ! command -v yay &> /dev/null; then
 fi
 
 packages=(
-  "ffmpeg htop wget git git-lfs openssh imagemagick curl usbutils udisks2 udiskie zip unzip unrar p7zip man-db man-pages texinfo networkmanager cronie libappindicator-gtk3 aardvark-dns apparmor cifs-utils fuse-overlayfs strace gst-plugin-pipewire gst-plugins-good mlocate" # Base
-  "librewolf-bin" # browser
+  "ffmpeg htop wget git git-lfs openssh imagemagick curl usbutils udisks2 udiskie zip unzip unrar p7zip man-db man-pages texinfo networkmanager cronie libappindicator-gtk3 aardvark-dns apparmor cifs-utils fuse-overlayfs strace gst-plugin-pipewire gst-plugins-good mlocate xdg-desktop-portal xdg-desktop-portal-wlr-git dunst playerctl python-pywal" # Base
+  "librewolf-bin rclone syncthing zerotier-one" # Internet apps
   "cups system-config-printer" # Printer
-  "sway swaylock-effects-git swaybg i3status-rust xorg-xwayland wl-clipboard wf-recorder clipmon-git rofi-lbonn-wayland-git xdg-desktop-portal xdg-desktop-portal-wlr-git grim slurp playerctl dunst python-pywal" # sway setup
+  "xorg-xrandr xorg-server xorg-xgamma xorg-xinit xclip i3-wm i3status-rust picom rofi parcellite" # i3 setup
   "alacritty zsh bat exa dust procs tmux fd fzf fselect-bin ripgrep neovim jq lsof urlview tldr" # terminal
   "podman podman-compose podman-docker podman-dnsname" # docker alternative
-  "rclone" # google drive mount and backup
   "nodejs node-gyp npm yarn elixir go jdk11-openjdk jdk8-openjdk" # development
   "mariadb-clients memcached pgcli php redis r ruby rust sqlite3 postgresql-libs" # development
   "helm python-pip python-numpy python-pandas python-pygments terraform terragrunt" # development
   "kubectl-bin kustomize openshift-client-bin skaffold-bin wrk" # development
-  "imv mpv mpv-mpris ncmpcpp mopidy mopidy-mpd yt-dlp" # media
-  "alsa-utils alsa-plugins pavucontrol pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-jack"
+  "imv mpv mpv-mpris ncmpcpp mopidy mopidy-mpd mopidy-ytmusic yt-dlp" # media
+  "alsa-utils alsa-plugins pavucontrol pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-jack" # audio
   "bluez bluez-utils" # bluetooth
   "zathura zathura-djvu zathura-pdf-mupdf zathura-ps zathura-cb" # PDF viewer
   "papirus-icon-theme-git" # Icon theme
-  "light gammastep kanshi wdisplays-git wshowkeys-git" # screen
+  "light redshift autorandr arandr" # screen
   "kdeconnect"
   "noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fontconfig" # fonts
   "thunar thunar-archive-plugin thunar-media-tags-plugin" # Thunar file explorer
@@ -49,6 +48,7 @@ packages=(
 
 # Install packages
 yay -S --noconfirm --needed ${packages[@]}
+# sudo python3 -m pip install Mopidy-YTMusic --break-system-packages
 
 # add user to groups
 sudo usermod -a -G wheel,video $(whoami)
@@ -60,5 +60,8 @@ systemctl --user enable pipewire.service pipewire.socket pipewire-pulse.service 
 systemctl enable --now tlp.service
 systemctl mask systemd-rfkill.service
 systemctl mask systemd-rfkill.socket
+
+# thermald prevent overheating on intel CPUs
+systemctl enable --now thermald
 
 systemctl enable --now cronie
