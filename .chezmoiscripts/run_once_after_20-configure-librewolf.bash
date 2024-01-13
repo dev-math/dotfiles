67518abaa4cf85fa02aeeec242dd81b836ce9447
Matheus Rocha <01devmath@gmail.com>
2024-01-13 02:04:36 -0300

    fix: userChrome.css should not be moved

--- .chezmoiscripts/run_once_after_20-configure-librewolf.bash
+++ .chezmoiscripts/run_once_after_20-configure-librewolf.bash
@@ -13,7 +13,7 @@ LIBREW_PROF_NAME="$(sed -n "/Default=.*.default-release/ s/.*=//p" "$LIBREW_CONF
 LIBREW_CHROME_DIR="$LIBREW_CONFIG_DIR/$LIBREW_PROF_NAME/chrome"
 
 mkdir -p "$LIBREW_CHROME_DIR"
-mv "$CHEZMOI_SOURCE_DIR/dot_config/userChrome.css" "$LIBREW_CHROME_DIR"
+cp "$CHEZMOI_SOURCE_DIR/dot_config/userChrome.css" "$LIBREW_CHROME_DIR"
 
 # Configure mpv firefox add-on
 xdg-mime default mpv-scheme-handler.desktop x-scheme-handler/mpv
