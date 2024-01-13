636d7c67af4a0ecc1ffc11ad81825a0eda02acf6
Matheus Rocha <01devmath@gmail.com>
2024-01-13 11:59:14 -0300

    feat: configure nvim transparency

--- dot_config/nvim/after/plugin/colorscheme.lua
+++ dot_config/nvim/after/plugin/colorscheme.lua
@@ -1,7 +1,9 @@
 require("NeoSolarized").setup({
 	enable_italics = false,
-	-- transparent = false,
+	transparent = true,
 	-- style = "light",
 })
 
-vim.cmd([[ colorscheme NeoSolarized ]])
+vim.cmd.colorscheme("NeoSolarized")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
