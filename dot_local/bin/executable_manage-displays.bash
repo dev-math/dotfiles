c4e9d2d06e07bf2299c23e727ee0480b0bcdc264
Matheus Rocha <01devmath@gmail.com>
2024-01-30 23:19:07 -0300

    fix: wrong comparation

--- dot_local/bin/executable_manage-displays.bash
+++ dot_local/bin/executable_manage-displays.bash
@@ -7,16 +7,20 @@ acpi_listen |
 while IFS= read -r event; do
     if [[ "$event" =~ "button/lid" ]]; then
         state=($event) && state="${state[-1]}"
-
+        echo "$state"
         if [[ "$state" == "open" ]]; then
             # turn on laptop_screen if its off
             xrandr --output "$laptop_screen" --auto 
-        elif [[ "$state" == "closed" ]]; then
+        elif [[ "$state" == "close" ]]; then
             # if only one display is connected then suspend the pc
             if [ $(xrandr | grep -i " connected" | wc -l) == 1 ]; then
+                # mute the system before sleeping
+                pactl set-sink-volume @DEFAULT_SINK@ 0
+                pactl set-sink-mute @DEFAULT_SINK@ true
+
                 # We leave a bit of time for locking to happen before putting 
                 # the system to sleep
-                ~/.local/bin/lockscreen && sleep 3
+                ~/.local/bin/lockscreen && sleep 1
                 if ! grep -q open /proc/acpi/button/lid/LID/state; then
                     systemctl suspend
                 fi
