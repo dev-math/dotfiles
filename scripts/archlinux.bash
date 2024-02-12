7b824f6f3811417c7ebba0a2062d496e2a2b322d
Matheus Rocha <01devmath@gmail.com>
2024-02-12 02:40:22 -0300

    feat: uninstall some flatpaks

--- scripts/archlinux.bash
+++ scripts/archlinux.bash
@@ -21,7 +21,7 @@ fi
 
 packages=(
   "ffmpeg htop wget git git-lfs openssh imagemagick curl usbutils udisks2 udiskie zip unzip unrar p7zip man-db man-pages texinfo networkmanager cronie libappindicator-gtk3 aardvark-dns apparmor cifs-utils fuse-overlayfs strace gst-plugin-pipewire gst-plugins-good mlocate xdg-desktop-portal xdg-desktop-portal-wlr-git dunst playerctl python-pywal" # Base
-  "librewolf-bin rclone syncthing zerotier-one" # Internet apps
+  "librewolf-bin torbrowser-launcher telegram-desktop rclone syncthing zerotier-one discord qbittorrent" # Internet apps
   "cups system-config-printer" # Printer
   "xorg-xrandr xorg-server xorg-xgamma xorg-xinit xclip i3-wm i3lock-color-git i3status-rust picom rofi parcellite" # i3 setup
   "alacritty zsh bat exa dust procs tmux fd fzf fselect-bin ripgrep neovim jq lsof urlview tldr" # terminal
@@ -30,7 +30,7 @@ packages=(
   "mariadb-clients memcached pgcli php redis r ruby rust sqlite lib32-sqlite postgresql-libs" # development
   "helm python-pip python-numpy python-pandas python-pygments terraform terragrunt" # development
   "kubectl-bin kustomize openshift-client-bin skaffold-bin wrk" # development
-  "imv mpv mpv-mpris ncmpcpp mopidy mopidy-mpd mopidy-ytmusic yt-dlp" # media
+  "imv mpv mpv-mpris ncmpcpp yt-dlp obs-studio flameshot-git" # media
   "alsa-utils alsa-plugins pavucontrol pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-jack" # audio
   "bluez bluez-utils" # bluetooth
   "zathura zathura-djvu zathura-pdf-mupdf zathura-ps zathura-cb" # PDF viewer
@@ -41,10 +41,11 @@ packages=(
   "thunar thunar-archive-plugin thunar-media-tags-plugin" # Thunar file explorer
   "mesa mesa-utils mesa-libgl lib32-mesa libvdpau-va-gl libva-mesa-driver vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader intel-media-driver intel-hybrid-codec-driver-git"
   # Wine dependencies
-  "wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error lib32-alsa-plugins libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs"
-  "lib32-gamemode gamemode"
+  "wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error lib32-alsa-plugins libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vkd3d lib32-vkd3d"
+  "lib32-gamemode gamemode steam lutris"
   "tlp" # battery
   "thermald intel-ucode" # Intel CPU
+  "gimp qalculate-gtk"
 )
 
 # Install packages
