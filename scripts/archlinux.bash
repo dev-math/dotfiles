5164e7f30ae55ee0b084b3be9bc7e87b2e5ab3fb
Matheus Rocha <01devmath@gmail.com>
2024-02-20 05:06:35 -0300

    feat: move to hyprland

--- scripts/archlinux.bash
+++ scripts/archlinux.bash
@@ -20,53 +20,54 @@ if ! command -v yay &> /dev/null; then
 fi
 
 packages=(
-  "ffmpeg htop wget git git-lfs openssh imagemagick curl usbutils udisks2 udiskie zip unzip unrar p7zip man-db man-pages texinfo networkmanager cronie libappindicator-gtk3 aardvark-dns apparmor cifs-utils fuse-overlayfs strace gst-plugin-pipewire gst-plugins-good mlocate xdg-desktop-portal xdg-desktop-portal-wlr-git dunst playerctl python-pywal" # Base
-  "librewolf-bin torbrowser-launcher telegram-desktop rclone syncthing zerotier-one discord qbittorrent" # Internet apps
-  "cups system-config-printer" # Printer
-  "xorg-xrandr xorg-server xorg-xgamma xorg-xinit xclip i3-wm i3lock-color-git i3status-rust picom rofi parcellite" # i3 setup
-  "alacritty zsh bat dust procs tmux fd fzf fselect-bin ripgrep neovim jq lsof urlview tldr" # terminal
-  "podman podman-compose podman-docker podman-dnsname" # docker alternative
-  "nodejs node-gyp npm yarn elixir go jdk11-openjdk jdk8-openjdk" # development
-  "mariadb-clients memcached pgcli php redis r ruby rust sqlite lib32-sqlite postgresql-libs" # development
-  "helm python-pip python-numpy python-pandas python-pygments terraform terragrunt" # development
-  "kubectl-bin kustomize openshift-client-bin skaffold-bin wrk" # development
-  "imv mpv mpv-mpris ncmpcpp yt-dlp obs-studio flameshot-git" # media
-  "alsa-utils alsa-plugins pavucontrol pipewire wireplumber pipewire-alsa pipewire-pulse pipewire-jack" # audio
-  "bluez bluez-utils" # bluetooth
-  "zathura zathura-djvu zathura-pdf-mupdf zathura-ps zathura-cb" # PDF viewer
-  "papirus-icon-theme-git" # Icon theme
-  "light redshift autorandr arandr" # screen
-  "kdeconnect"
-  "noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fontconfig" # fonts
-  "thunar thunar-archive-plugin thunar-media-tags-plugin" # Thunar file explorer
-  "mesa mesa-utils mesa-libgl lib32-mesa libvdpau-va-gl libva-mesa-driver vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader intel-media-driver intel-hybrid-codec-driver-git"
+  # base
+  "ffmpeg imagemagick usbutils udisks2 udiskie zip unzip unrar p7zip man-db"
+  "man-pages texinfo networkmanager cronie libappindicator-gtk3 aardvark-dns"
+  "apparmor cifs-utils fuse-overlayfs strace gst-plugin-pipewire"
+  "gst-plugins-good mlocate"
+  # drivers
+  "mesa mesa-utils mesa-libgl lib32-mesa libvdpau-va-gl libva-mesa-driver"
+  "vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader"
+  "intel-media-driver intel-hybrid-codec-driver-git"
+  "tlp thermald intel-ucode"
+  # rice 
+  "hyprland-git waybar hyprpaper light gammastep kanshi nwg-displays"
+  "xdg-desktop-portal-hyprland-git xdg-desktop-portal-termfilechooser-git"
+  "wshowkeys-git python-pywal dunst wlr-randr-git rofi-lbonn-wayland-git"
+  "qt5-wayland qt6-wayland"
+  # development
+  "podman podman-compose podman-docker"
+  "nodejs node-gyp npm yarn elixir go jdk11-openjdk jdk8-openjdk pgcli php ruby"
+  "mariadb-clients memcached redis r rust sqlite lib32-sqlite postgresql-libs"
+  "python-pip python-numpy python-pandas python-pygments wrk"
+  # terminal
+  "wget git git-lfs openssh curl htop zsh exa bat dust procs tmux fd fzf"
+  "foot fselect-bin ripgrep neovim jq lsof urlview tldr nnn-nerd"
+  # internet
+  "librewolf-bin torbrowser-launcher telegram-desktop rclone syncthing kdeconnect"
+  "zerotier-one discord"
+  # Printer
+  "cups system-config-printer"
+  # bluetooth
+  "bluez bluez-cups bluez-utils"
+  # media
+  "zathura zathura-djvu zathura-pdf-mupdf zathura-ps zathura-cb"
+  "gimp inkscape krita xournalpp"
+  "imv playerctl mpv mpv-mpris ncmpcpp yt-dlp obs-studio"
+  "alsa-utils alsa-plugins pavucontrol pipewire wireplumber pipewire-alsa"
+  "pipewire-pulse pipewire-jack"
+  # fonts
+  "noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fontconfig"
+  # games
+  "lib32-gamemode gamemode steam lutris"
+  # misc
+  "qalculate-gtk"
   # Wine dependencies
   "wine-staging giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error lib32-alsa-plugins libjpeg-turbo lib32-libjpeg-turbo libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses ocl-icd lib32-ocl-icd libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vkd3d lib32-vkd3d"
-  "lib32-gamemode gamemode steam lutris"
-  "tlp" # battery
-  "thermald intel-ucode" # Intel CPU
-  "gimp qalculate-gtk"
 )
 
 # Install packages
 yay -S --noconfirm --needed ${packages[@]}
-# sudo python3 -m pip install Mopidy-YTMusic --break-system-packages
 
 # add user to groups
-sudo usermod -a -G wheel,video,audio,gamemode $(whoami)
-
-# enable pipewire
-systemctl --user enable pipewire.service pipewire.socket pipewire-pulse.service wireplumber.service
-
-# enable tlp for battery power saving
-systemctl enable --now tlp.service
-systemctl mask systemd-rfkill.service
-systemctl mask systemd-rfkill.socket
-
-# thermald prevent overheating on intel CPUs
-systemctl enable --now thermald
-
-systemctl enable --now cronie
-
-# disable systemd lid events
-sed -e 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/g' -e 's/#HandleLidSwitchExternalPower=suspend/HandleLidSwitchExternalPower=ignore/g' /etc/systemd/logind.conf > tmp_file && sudo mv tmp_file /etc/systemd/logind.conf
+sudo usermod -a -G video,audio,gamemode $(whoami)
