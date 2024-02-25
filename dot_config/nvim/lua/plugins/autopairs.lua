f176c90f92d1a7c38b72c105ec3da130ccae08ba
Matheus Rocha <01devmath@gmail.com>
2024-02-25 01:01:09 -0300

    feat: configure autopairs plugin

--- dot_config/nvim/lua/plugins/autopairs.lua
+++ dot_config/nvim/lua/plugins/autopairs.lua
@@ -2,4 +2,21 @@ return {
   "windwp/nvim-autopairs",
   event = "InsertEnter",
   opts = {},
+  config = function()
+    local autopairs = require("nvim-autopairs")
+
+    local cmp = require("cmp")
+    autopairs.setup({
+      check_ts = true,
+      ts_config = {
+        lua = { "string", "source" },
+        javascript = { "string", "template_string" },
+        java = false,
+      },
+      disable_filetype = { "TelescopePrompt", "vim" },
+      fast_wrap = {},
+    })
+    local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done({ map_char = { tex = "" } }))
+  end,
 }
