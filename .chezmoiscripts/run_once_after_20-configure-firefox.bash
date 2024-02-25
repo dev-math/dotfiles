b3dfc1098d0273786672d12407a59388a227317b
Matheus Rocha <01devmath@gmail.com>
2024-02-25 17:05:48 -0300

    feat: set script to work on cachy-browser

--- .chezmoiscripts/run_once_after_20-configure-firefox.bash
+++ .chezmoiscripts/run_once_after_20-configure-firefox.bash
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 set -o errexit -o nounset
-# Start librewolf in headless mode in order to create config directory for it.
-mkdir -p $HOME/.librewolf
-librewolf --headless >/dev/null 2>&1 &
+# Start cachy in headless mode in order to create config directory for it.
+mkdir -p $HOME/.cachy
+cachy-browser --headless >/dev/null 2>&1 &
 sleep 3
-killall librewolf
+killall cachy-browser
 
 # Find the profile folder.
-LIBREW_CONFIG_DIR="$HOME/.librewolf"
+LIBREW_CONFIG_DIR="$HOME/.cachy"
 LIBREW_PROF_NAME="$(sed -n "/Default=.*.default-release/ s/.*=//p" "$LIBREW_CONFIG_DIR/profiles.ini")"
 LIBREW_CHROME_DIR="$LIBREW_CONFIG_DIR/$LIBREW_PROF_NAME/chrome"
 
