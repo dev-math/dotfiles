5eb746daf043d10a642db7c0e181904125330751
Matheus Rocha <01devmath@gmail.com>
2024-02-25 17:06:01 -0300

    feat: add more packages

--- scripts/archlinux.bash
+++ scripts/archlinux.bash
@@ -20,16 +20,37 @@ if ! command -v yay &> /dev/null; then
 fi
 
 packages=(
-  # base
-  "ffmpeg imagemagick usbutils udisks2 udiskie zip unzip unrar p7zip man-db"
-  "man-pages texinfo networkmanager cronie libappindicator-gtk3 aardvark-dns"
-  "apparmor cifs-utils fuse-overlayfs strace gst-plugin-pipewire"
-  "gst-plugins-good mlocate electron electron29-bin"
+  # utils
+  "accountsservice ffmpegthumbnailer gst-libav gst-plugin-pipewire gst-plugins-bad"
+  "gst-plugins-ugly libdvdcss libgsf libopenraw mlocate poppler-glib xdg-user-dirs"
+  "xdg-utils imagemagick usbutils udisks2 udiskie"
+  "man-db man-pages texinfo cronie apparmor cifs-utils fuse-overlayfs strace"
+  "electron electron29-bin hwinfo"
+  # filesystem
+  "efitools haveged nfs-utils nilfs-utils ntp smartmontools unrar unzip xz"
+  # hardware libs
+  "dmidecode dmraid hdparm hwdetect lsscsi mtools sg3_utils sof-firmware"
+  # power
+  "upower cpupower power-profiles-daemon"
+  # audio
+  "alsa-firmware alsa-utils alsa-plugins pavucontrol pipewire wireplumber"
+  "pipewire-alsa pipewire-pulse pipewire-jack rtkit"
+  # networks
+  "networkmanager aardvark-dns"
+  # bluetooth
+  "bluez bluez-cups bluez-utils"
   # drivers
-  "mesa mesa-utils mesa-libgl lib32-mesa libvdpau-va-gl libva-mesa-driver"
-  "vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader"
-  "intel-media-driver intel-hybrid-codec-driver-git"
-  "tlp thermald intel-ucode"
+  # "mesa mesa-utils mesa-libgl lib32-mesa libvdpau-va-gl libva-mesa-driver"
+  # "vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader"
+  # "intel-media-driver intel-ucode"
+  "tlp thermald"
+  # fonts
+  "noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fontconfig"
+  # Printer
+  "cups cups-filters cups-pdf foomatic-db foomatic-db-engine foomatic-db-gutenprint-ppds"
+  "foomatic-db-nonfree foomatic-db-nonfree-ppds foomatic-db-ppds ghostscript gsfonts"
+  "gutenprint splix system-config-printer"
+  "hplip python-pyqt5 python-reportlab xsane"
   # rice 
   "hyprland-git waybar swaybg swaylock-effects-git light wlsunset kanshi nwg-displays"
   "xdg-desktop-portal-hyprland xdg-desktop-portal-termfilechooser-git"
@@ -42,23 +63,15 @@ packages=(
   "mariadb-clients memcached redis r rust sqlite lib32-sqlite postgresql-libs"
   "python-pip python-numpy python-pandas python-pygments wrk"
   # terminal
-  "wget git git-lfs openssh curl htop zsh exa bat dust procs tmux fd fzf"
+  "wget ripgrep git git-lfs openssh curl htop zsh exa bat dust procs tmux fd fzf"
   "foot fselect-bin ripgrep neovim jq lsof urlview tldr nnn-nerd"
   # internet
   "librewolf-bin torbrowser-launcher telegram-desktop rclone syncthing kdeconnect"
   "zerotier-one discord webcord-git"
-  # Printer
-  "cups system-config-printer"
-  # bluetooth
-  "bluez bluez-cups bluez-utils"
   # media
   "zathura zathura-djvu zathura-pdf-mupdf zathura-ps zathura-cb"
   "gimp inkscape krita xournalpp"
   "imv playerctl mpv mpv-mpris ncmpcpp yt-dlp obs-studio"
-  "alsa-utils alsa-plugins pavucontrol pipewire wireplumber pipewire-alsa"
-  "pipewire-pulse pipewire-jack"
-  # fonts
-  "noto-fonts noto-fonts-cjk noto-fonts-emoji noto-fonts-extra fontconfig"
   # games
   "lib32-gamemode gamemode steam lutris"
   # misc
