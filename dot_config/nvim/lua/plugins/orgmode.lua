9d38fddcd0e5687c8975a182abc4cfa88d2a4b0b
Matheus Rocha <01devmath@gmail.com>
2024-04-29 07:03:45 -0300

    refactor: treesitter dependencie removed

--- dot_config/nvim/lua/plugins/orgmode.lua
+++ dot_config/nvim/lua/plugins/orgmode.lua
@@ -1,23 +1,20 @@
 return {
-  "nvim-orgmode/orgmode",
-  dependencies = {
-    { "nvim-treesitter/nvim-treesitter", lazy = true },
-    "akinsho/org-bullets.nvim",
-  },
+	"nvim-orgmode/orgmode",
+	dependencies = {
+		"akinsho/org-bullets.nvim",
+	},
 
-  config = function()
-    -- Load treesitter grammar for org
-    require("orgmode").setup_ts_grammar()
-    -- Setup orgmode
-    require("orgmode").setup({
-      org_agenda_files = "~/Syncthing/notes/**/*",
-      org_default_notes_file = "~/Syncthing/notes/refile.org",
-      org_deadline_warning_days = 5,
-      org_agenda_start_on_weekday = 7,
-      org_highlight_latex_and_related = "native",
-      notifications = { enabled = true },
-    })
-    -- replaces the asterisks in org syntax with unicode characters.
-    require('org-bullets').setup()
-  end
+	config = function()
+		-- Setup orgmode
+		require("orgmode").setup({
+			org_agenda_files = "~/Syncthing/notes/**/*",
+			org_default_notes_file = "~/Syncthing/notes/refile.org",
+			org_deadline_warning_days = 5,
+			org_agenda_start_on_weekday = 7,
+			org_highlight_latex_and_related = "native",
+			notifications = { enabled = true },
+		})
+		-- replaces the asterisks in org syntax with unicode characters.
+		require("org-bullets").setup()
+	end,
 }
