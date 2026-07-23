1f216f7c20fe019f0c55015cab1b1786bab1d6a7
Matheus Rocha <01devmath@gmail.com>
2026-07-22 21:23:18 -0300

    feat: debian 13 ajustes

--- .chezmoiscripts/run_once_after_20-configure-firefox.bash
+++ .chezmoiscripts/run_once_after_20-configure-firefox.bash
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 set -o errexit -o nounset
-# Start cachy in headless mode in order to create config directory for it.
-mkdir -p $HOME/.cachy
-cachy-browser --headless >/dev/null 2>&1 &
+# Start firefox in headless mode in order to create config directory for it.
+mkdir -p $HOME/.mozilla
+firefox --headless >/dev/null 2>&1 &
 sleep 3
-killall cachy-browser
+echo "kill firefox before continuing"
+# pkill -f firefox
 
 # Find the profile folder.
-LIBREW_CONFIG_DIR="$HOME/.cachy"
-LIBREW_PROF_NAME="$(sed -n "/Default=.*.default-release/ s/.*=//p" "$LIBREW_CONFIG_DIR/profiles.ini")"
+LIBREW_CONFIG_DIR="$HOME/.mozilla/firefox"
+LIBREW_PROF_NAME="$(sed -n "/Default=.*.default/ s/.*=//p" "$LIBREW_CONFIG_DIR/profiles.ini")"
 LIBREW_CHROME_DIR="$LIBREW_CONFIG_DIR/$LIBREW_PROF_NAME/chrome"
 
 mkdir -p "$LIBREW_CHROME_DIR"
@@ -17,6 +18,3 @@ cp "$CHEZMOI_SOURCE_DIR/dot_config/userChrome.css" "$LIBREW_CHROME_DIR"
 
 # Configure mpv firefox add-on
 xdg-mime default mpv-scheme-handler.desktop x-scheme-handler/mpv
-
-# install Tridactyl native messenger
-curl -fsSl https://raw.githubusercontent.com/tridactyl/native_messenger/master/installers/install.sh -o /tmp/trinativeinstall.sh && sh /tmp/trinativeinstall.sh 1.24.0
---
+++ scripts/install_i3status-rust.bash
@@ -0,0 +1,4 @@
+git clone https://github.com/greshake/i3status-rust /tmp/i3status
+cd /tmp/i3status
+cargo install --path . --locked
+./install.sh
